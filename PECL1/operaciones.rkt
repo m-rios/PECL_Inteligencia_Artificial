eda6495061bbd5eaaf1557cb747d8ae4e340daa1
Mario Rios <mrm994@gmail.com>
2015-04-14 17:17:51 +0200

    parámetros completamente dinámicos

--- PECL1/operaciones.rkt
+++ PECL1/operaciones.rkt
@@ -1,67 +1,100 @@
 #lang racket
-(define m 3)
-(define n 4)
-(define p 2)
 
 ;funciones auxiliares
+;===============================================================================
 (define (get_m nodo) (car nodo))
-(define (get_n nodo) (car (cdr nodo)))
+(define (get_n nodo) (cadr nodo))
+(define (get_p nodo) (caddr nodo))
 (define (param_validos m n p) (and (< m n) (<= p m)))
 
 ;operaciones
-(define (llenar_m nodo) (list m (car (cdr nodo))))
-(define (llenar_n nodo) (list (car nodo) n))
+;===============================================================================
+(define (llenar_m param nodo) (list (get_m param) (car (cdr nodo))))
+(define (llenar_n param nodo) (list (car nodo) (get_n param)))
 
-(define (vaciar_m nodo) (list 0 (car (cdr nodo))))
-(define (vaciar_n nodo) (list (car nodo) 0))
+(define (vaciar_m param nodo) (list 0 (car (cdr nodo))))
+(define (vaciar_n param nodo) (list (car nodo) 0))
 
-(define (volcar_m nodo)
-    (if (>= (- n (get_n nodo)) (get_m nodo))
+(define (volcar_m param nodo)
+    (if (>= (- (get_n param) (get_n nodo)) (get_m nodo))
         (list 0 (+ (get_m nodo) (get_n nodo)))
-        (list (- (get_m nodo) (- n (get_n nodo))) n)))
-(define (volcar_n nodo)
-    (if (>= (- m (get_m nodo)) (get_n nodo))
+        (list (- (get_m nodo) (- (get_n param) (get_n nodo))) (get_n param))))
+(define (volcar_n param nodo)
+    (if (>= (- (get_m param) (get_m nodo)) (get_n nodo))
         (list (+ (get_m nodo) (get_n nodo)) 0)
-        (list m (- (get_n nodo) (- m (get_m nodo))))))
+        (list (get_m param) (- (get_n nodo) (- (get_m param) (get_m nodo))))))
 
+;calculo de sucesores
+;===============================================================================
 ;devuelve los sucesores que no han sido evaluados aún
 (define (crear_supernodos padre nodos supernodos)
     (if (empty? nodos) supernodos
         (crear_supernodos padre 
             (cdr nodos) 
             (append supernodos (list (list (car nodos) padre))))))
-(define (sucesores nodo cerrados) 
-    (crear_supernodos nodo (remove* cerrados (sucesores_aux nodo)) null))
+(define (sucesores param nodo cerrados) 
+    (crear_supernodos nodo (remove* cerrados (sucesores_aux param nodo)) null))
 
 ;calcula todos los sucesores de un nodo
-(define (sucesores_aux nodo) (list (llenar_m nodo) (llenar_n nodo) 
-    (vaciar_m nodo) (vaciar_n nodo) (volcar_m nodo) (volcar_n nodo)))
+(define (sucesores_aux param nodo) (list (llenar_m param nodo) 
+    (llenar_n param nodo) (vaciar_m param nodo) (vaciar_n param nodo) 
+    (volcar_m param nodo) (volcar_n param nodo)))
 
 ;funciones del algoritmo
 ;===============================================================================
 ;cerrados solo almacena el nodo, no el padre
 ;abiertos almacena el nodo y su padre (list nodo padre)
 ;arbol almacena el nodo y su padre (list nodo padre)
-(define (algoritmo abiertos cerrados arbol)
-    (if (= p (get_m (caar abiertos))) (append (list (car abiertos)) arbol)
-        (algoritmo (append (cdr abiertos) (sucesores (caar abiertos) 
+(define (algoritmo param abiertos cerrados arbol)
+    (if (= (get_p param) (get_m (caar abiertos))) (append (list (car abiertos)) arbol)
+        (algoritmo param (append (cdr abiertos) (sucesores param (caar abiertos) 
             (append cerrados (list (caar abiertos)))))
          (append cerrados (list (caar abiertos))) 
          (append (list (car abiertos)) arbol))
         )
     )
 
-;devuelve la sublista que sigue al nodo
+;devuelve la sublista que sigue a 'nodo'
 (define (buscar nodo arbol) 
     (if (eq? (caar arbol) nodo) arbol
         (buscar nodo (cdr arbol))))
 
+;obtiene la rama del arbol que lleva a la solucion
 (define (calcular_ruta arbol ruta)
     (if (empty? (cadar arbol)) (append (list (caar arbol)) ruta)
         (calcular_ruta (buscar (cadar arbol) (cdr arbol))
             (append (list (caar arbol)) ruta))))
 
-(define resolver (calcular_ruta (algoritmo (list (list (list 0 0) null)) 
-    empty empty) null))
+;indica las operaciones realizadas sobre cada nodo
+(define (modificaciones ruta salida)
+    (if (empty? (cdr ruta)) (append salida (list (car ruta)))
+        (cond 
+            [(equal? (llenar_m (car ruta)) (cadr ruta)) =>
+            (modificaciones (cdr ruta) 
+                (append salida (list (car ruta) 'llenar_m)))]
+            [(equal? (llenar_n (car ruta)) (cadr ruta)) =>
+                        (modificaciones (cdr ruta) 
+                            (append salida (list (car ruta) 'llenar_n)))]
+            [(equal? (vaciar_m (car ruta)) (cadr ruta)) =>
+                        (modificaciones (cdr ruta) 
+                            (append salida (list (car ruta) 'vaciar_m)))]
+            [(equal? (vaciar_n (car ruta)) (cadr ruta)) =>
+                        (modificaciones (cdr ruta) 
+                            (append salida (list (car ruta) 'vaciar_n)))]
+            [(equal? (volcar_m (car ruta)) (cadr ruta)) =>
+                        (modificaciones (cdr ruta) 
+                            (append salida (list (car ruta) 'volcar_m)))]
+            [(equal? (volcar_n (car ruta)) (cadr ruta)) =>
+                        (modificaciones (cdr ruta) 
+                            (append salida (list (car ruta) 'volcar_n)))]
+            )
+        ))
 
-(define test (algoritmo (list (list (list 1 1) null)) empty empty))
+;manejadores
+;===============================================================================
+;i_m y i_n definen el estado inicial
+(define (resolver m n p i_m i_n)
+    (if (param_validos m n p) (calcular_ruta (algoritmo (list m n p) 
+        (list(list (list i_m i_n) null)) null null) null)
+        (printf "Parámetros no válidos")
+        ))
