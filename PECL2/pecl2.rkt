08aae106d901530a2f8c9029af9851177906df17
Mario Rios <mrm994@gmail.com>
2015-05-18 19:38:40 +0200

    ya calcula bien los nodos, algoritmo plantar acabado
    ahora hay que implementar las jugadas

--- PECL2/pecl2.rkt
+++ PECL2/pecl2.rkt
@@ -1,6 +1,4 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname pecl2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+#lang racket
 ;devuelve el tamaño en mm de cada subdivisión
 (define (dinA_mm n)
   (map (lambda (number1 number2)
@@ -40,53 +38,59 @@
 (define (max-l lista) (car (sort lista >)))
 ;calcula los hijos de un nodo del árbol de búsqueda
 (define (explotar nodo)
-  (append (explotar-izq nodo '() (first (get-tab nodo))) ;hijos jugando por la izquierda
-          (explotar-der nodo '() (second (get-tab nodo))) ;hijos jugando por la derecha
+  (append (explotar-izq nodo '() (get-minmax nodo)) ;hijos jugando por la izquierda
+          (explotar-der nodo '() (get-minmax nodo)) ;hijos jugando por la derecha
           (list (list (get-tab nodo) (get-minmax nodo) -1 (get-pun nodo)));mismo nodo, para hacer backtracking
           ))
-(define (explotar-izq nodo hijos max)
+(define (explotar-izq nodo hijos minmax)
   (if (= 1 (first (get-tab nodo)))
       hijos
-      ((lambda (x) (explotar-izq x (append hijos (list x)) max)) 
+      ((lambda (x) (explotar-izq x (append hijos (list x)) minmax)) 
        (list 
         (list (- (first (get-tab nodo)) 1) (second (get-tab nodo)));tab
-        (remainder (+ 1 (get-minmax nodo)) 2); min-max
+        (remainder (+ 1 minmax) 2); min-max
         -2 ;no explotado
         -1 ;no tiene puntuacion
         ))
       ))
 
-(define (explotar-der nodo hijos max)
+(define (explotar-der nodo hijos minmax)
   (if (= 1 (second (get-tab nodo)))
       hijos
-      ((lambda (x) (explotar-der x (append hijos (list x)) max)) 
+      ((lambda (x) (explotar-der x (append hijos (list x)) minmax)) 
        (list 
         (list (first (get-tab nodo)) (- (second (get-tab nodo)) 1));tab
-        (remainder (+ 1 (get-minmax nodo)) 2); min-max
+        (remainder (+ 1 minmax) 2); min-max
         -2 ;no explotado
         -1 ;no tiene puntuacion
         ))
-      ))                                                                                     
-;crea el árbol de decisiones 
+      ))  
+;obtiene la puntuación de un nodo intermedio
+(define (puntuar nodo puntuaciones)
+  (if (= 0 (get-minmax nodo))
+      (max-l puntuaciones);si max, devuelve el máximo de los hijos
+      (min-l puntuaciones);si min, devuelve el mínimo de los hijos
+      ))
+;crea el árbol de decisiones  
 (define (plantar-aux actual abiertos puntuaciones arbol)
   (if (and (empty? abiertos) (not(empty? arbol))) ;si abiertos vacio y arbol no vacio, casi se ha terminado el árbol
       (cons 
-       (list (get-tab actual) (get-minmax actual) (min-l (car puntuaciones))) arbol)    ;igual en vez de (list arbol) es arbol solo
+       (list (get-tab actual) (get-minmax actual) (get-tipo actual) (puntuar actual (car puntuaciones))) arbol)
       (if (equal? '(1 1) (get-tab actual))
           (plantar-aux (car abiertos);actual'
                    (cdr abiertos);abiertos'
                    (cons (cons (get-minmax actual) (car puntuaciones))
                          (cdr puntuaciones));se añade puntuación actual a la puntuación del nivel
-                   (cons (list (get-tab actual) (get-minmax actual) (get-minmax actual)) arbol);se añade nodo actual al arbol
+                   (cons (list (get-tab actual) (get-minmax actual) (get-tipo actual) (get-minmax actual)) arbol);se añade nodo actual al arbol
                    )
           (if (= (get-tipo actual) -1) ;no puntuado
               (plantar-aux (car abiertos);actual'
                        (cdr abiertos);abiertos'
                        ;calcular puntuación del nodo en función de sus hijos, y añadir
                        ;esa puntuación a la lista de puntuaciones del nivel del nodo
-                       (cons (cons (min-l (car puntuaciones)) (cadr puntuaciones))                                        ;en vez de min hay que hacer minmax
+                       (cons (cons (puntuar actual (car puntuaciones)) (cadr puntuaciones))
                              (cddr puntuaciones))
-                       (cons (list (get-tab actual) (get-minmax actual) (min-l (car puntuaciones))) arbol)) ;se añade el nodo puntuado
+                       (cons (list (get-tab actual) (get-minmax actual) (get-tipo actual) (min-l (car puntuaciones))) arbol)) ;se añade el nodo puntuado
               ;si no, tiene que ser no explotado (tipo = -2)
               ((lambda (x) (plantar-aux (car x);actual'
                        (cdr x) ;se añaden hijos menos el primero para busqueda en profundidad
@@ -98,11 +102,17 @@
       )
   )
 ;inicializa los parámetros para el algoritmo
+;al borrar los repetidos, devuelve los distintos nodos que pueden aparecer en
+;el arbol, con su valor (ganar/perder) asociado, para apoyar a la toma
+;de decisiones por parte de la máquina
 (define (plantar n) 
   (if (= n 0)
       "no tiene sentido jugar si no hay divisiones"
-      (plantar-aux (list (dinA n) 0 -2 -1) '() '() '()))
+      (remove-duplicates (plantar-aux (list (dinA n) 0 -2 -1) '() '() '())))
   )
+;inicializa los parámetros para jugar-aux
+(define (jugar n)
+  (jugar-aux true (list (dinA n) 0 -2 -1) (plantar n)))
 ;algoritmo que implementa las jugadas
 (define (jugar-aux maquina nodo arbol)
   (if (equal? (first nodo) '(1 1))
@@ -114,4 +124,3 @@
 ;jugada de máquina
 (define (jugada opciones arbol)
   false)
-(explotar-der '((4 4) 0 0 0) '() 4)
